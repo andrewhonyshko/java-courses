21a267566b4790c8e352d347fdfd48b253889462
ГОНИШКО Андрей Петрович <angol@sc.court.local>
2020-02-27 16:19:49 +0200

    all calculate

--- Calculate.java
+++ Calculate.java
@@ -5,6 +5,21 @@ public class Calculate{
 		int second=Integer.valueOf(arg[1]);
 		int sum=first+second;
 		System.out.println("Sum = "+sum);
+		
+		long flong=Long.valueOf(arg[2]);
+		long slong=Long.valueOf(sum);
+		long minus=slong-flong;
+		System.out.println("Minus = "+minus);
+		
+		float oneFloat=Float.valueOf(arg[3]);
+		float twoFloat=Float.valueOf(minus);
+		float dev=twoFloat/oneFloat;
+		System.out.println("DEV = "+dev);
+		
+		double oneDouble=Double.valueOf(arg[4]);
+		double twoDouble=Double.valueOf(dev);
+		double multiple=twoDouble*oneDouble;
+		System.out.println("Multiple = "+multiple);
 	}
 
 }
