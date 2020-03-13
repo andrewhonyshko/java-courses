9342237ebb112bf90a50dc562e7e67486a648300
ГОНИШКО Андрей Петрович <angol@sc.court.local>
2020-03-13 11:17:49 +0200

    all ariphmetic operation

--- Calculator.java
+++ Calculator.java
@@ -1,15 +1,84 @@
+import java.util.ArrayList;
 public class Calculator{
 	
 	private int result;
+	private ArrayList<Integer> params;
 	
-	public void add(int ... params)
+	public Calculator(ArrayList<Integer> listArgs)
 	{
+		this.params=listArgs;
+	}
+	
+	//public void add(ArrayList<Integer> params)
+	//{
+		//this.listArgs=params;
+	//	for(Integer param:params)
+		//{
+			//this.result+=param;
+		//}
+		
+	//}
+	public void add()
+	{
+		//this.listArgs=params;
 		for(Integer param:params)
 		{
 			this.result+=param;
 		}
 		
 	}
+	public void minus()
+	{
+		int i=0;
+		for(Integer param:params)
+		{
+			if(i==0)
+			{	
+				this.result=param;
+			}
+			else
+			{
+				this.result-=param;
+			}
+			i++;
+			
+		}
+	}
+	public void multiply()
+	{
+		int i=0;
+		for(Integer param:params)
+		{
+			if(i==0)
+			{	
+				this.result=param;
+			}
+			else
+			{
+				this.result*=param;
+			}
+			i++;
+			
+		}
+	}
+	public void div()
+	{
+		int i=0;
+		for(Integer param:params)
+		{
+			if(i==0)
+			{	
+				this.result=param;
+			}
+			else
+			{
+				this.result/=param;
+			}
+			i++;
+			
+		}
+	}
+	
 	public int getResult()
 	{
 		return this.result;
@@ -18,5 +87,42 @@ public class Calculator{
 	{
 		this.result=0;
 	}
+	public boolean isInteger(String strParam)
+	{
+		if(strParam==null)
+			return false;
+		try
+		{
+			int intParam=Integer.parseInt(strParam);
+		}
+		catch(NumberFormatException nfe)
+		{
+			return false;
+		}
+		return true;
+	}
+	public void choiceOperation(String numOperation,ArrayList<Integer> params)
+	{
+		
+		
+		//String numOperation=scan.next();
+		
+		switch(numOperation)
+		{
+			case "1":
+				add();
+				break;
+			case "2":
+				this.minus();
+				break;
+			case "3":
+				multiply();
+				break;
+			case "4":
+				div();
+				break;
+		}
+	}
+	
 	
 }
--- InteructRunner.java
+++ InteructRunner.java
@@ -1,25 +1,59 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+
+
 
 public class InteructRunner{
 	
 	public static void main(String[] arg)
 	{
 		Scanner scan=new Scanner(System.in);
+		ArrayList<Integer> listArgs;
 		
 		try{
 			String exit="no";
-			Calculator calc=new Calculator();
+			listArgs = new ArrayList<>();
+			Calculator calc=new Calculator(listArgs);
+			String argument="";
+			int argInt;
+			
+			//System.out.prinln("Choice operation\n 1:");
 			while(!exit.equals("yes"))
 			{
-				System.out.println("Please, enter first args");
-				String first=scan.next();
-				System.out.println("Please, enter second args");
-				String second=scan.next();
-				calc.add(Integer.valueOf(first),Integer.valueOf(second));
-				System.out.println("Result = "+calc.getResult());
+				//listArgs.clear();
+				int i=1;
 				
+				while(!argument.equals("stop"))
+				{
+					System.out.println("Please, enter " + i + " args or stop");
+					argument=scan.next();
+					if(calc.isInteger(argument))
+					{
+							argInt=Integer.valueOf(argument);
+							listArgs.add(argInt);
+							i++;
+							
+					}
+					
+				}
+				
+				String choiceString="Please, choice operation:"
+										+System.lineSeparator()+"	1. add"
+										+System.lineSeparator()+"	2. minus"
+										+System.lineSeparator()+"	3. multiply"
+										+System.lineSeparator()+"	4. div";
+							
+				System.out.println(choiceString);
+				String choice=scan.next();
+				calc.choiceOperation(choice,listArgs);
+				
+				
+				System.out.println("Result = "+calc.getResult());
+				listArgs.clear();
 				calc.clearResult();
+				
 				System.out.println("yes/no");
+				argument="";
 				exit=scan.next();
 			}
 			
